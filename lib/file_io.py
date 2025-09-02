d040aaaeb4b9987efd61679026852053048b90f6
ryangachahic <ryangachahi230@gmail.com>
2025-09-02 08:32:34 +0300

    intial commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
-def write_file(file_name, file_content):
-    pass
+# lib/file_io.py
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    """
+    Write content to a .txt file, overwriting any existing content.
+    """
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
+
+def append_file(file_name, file_content):
+    """
+    Append content to a .txt file.
+    """
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(file_content)
+
 
 def read_file(file_name):
-    pass
+    """
+    Read and return the content of a .txt file.
+    """
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
